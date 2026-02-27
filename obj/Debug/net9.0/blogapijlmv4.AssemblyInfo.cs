71f4a8ff362ce69051e8b48de137c7e1b00a7cfa
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2026-02-27 14:48:08 -0800

    Added EF core pacakges

--- obj/Debug/net9.0/blogapijlmv4.AssemblyInfo.cs
+++ obj/Debug/net9.0/blogapijlmv4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blogapijlmv4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74fbf38f8ba92639abe31a1f8518017ef0c651cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("blogapijlmv4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blogapijlmv4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
